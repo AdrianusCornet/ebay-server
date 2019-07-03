06fba149971c9f8ae7379c28055f37b633376a85
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-03 14:42:11 +0200

    get adds

--- add/router.js
+++ add/router.js
@@ -3,6 +3,16 @@ const Add = require('./model')
 
 const router = new Router
 
-//...
+router.get(
+  '/adds',
+  (request, response, next) => Add
+    .findAll()
+    .then(adds => {
+      console.log('bla');
+      
+      response.send({ adds })
+    })
+    .catch(next)
+)
 
 module.exports = router
